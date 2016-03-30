c2570b4c309f1f8fab1639793f9d80c995c336e3
Tomas Pimentel <Tomashacksforlife@gmail.com>
2016-03-29 20:13:18 -0400

    Update Tamcru.bat
    Added Run As Admin

--- Tamcru.bat
+++ Tamcru.bat
@@ -1,5 +1,21 @@
 @echo off
 Title *Toms Antivirus Multi Cleanup Repair Utility*
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
 :Ask
 cls
 echo                   Toms AntiVirus Multi Cleanup Repair Utility
